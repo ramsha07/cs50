6f92714c24303ce6eb047fd71bd967c9dd7a8193
Ramsha Aijaz <64930853+ramsha07@users.noreply.github.com>
2023-05-12 15:40:14 +0500

    Update credit.c

--- week1/credit/credit.c
+++ week1/credit/credit.c
@@ -3,66 +3,52 @@
 
 int main(void)
 {
-    long number = 1234;
-    int sum;
-    int digit;
-    int multiply1;
-    int multiply;
-    int multiply2;
-
-    while (number > 0)
+    long cardNumber = 378282246310005;
+    int sum = 0;
+    while (cardNumber > 0)
     {
-        int digit1 = number % 10;//4
-        number = number / 10;//123
-        multiply = number % 10 * 2;//3*2=6
-          printf("%d\n",multiply);
-    //     if (multiply >= 10)
-    //     {
-    //         multiply1 = multiply % 10;
-    //         multiply2 = multiply / 10;
-    //         sum = multiply1 + multiply2 % 10+digit1;
-    //         // sum =+sum;
-    //         printf ("%i\n", sum);
-    //     }
-    //     else
-    //     {
-    //           sum = multiply+ digit1;
-    //     sum =+sum;
-    //     }
-        number = number / 10;//123/10 =12
-
+        int firstDigit = cardNumber % 10; // 4
+        cardNumber = cardNumber / 10;     // 123
+        int secondDigit = cardNumber % 10 * 2;
+        // Logic for second Digit
+        sum = sum + secondDigit % 10;
+        secondDigit = secondDigit / 10;
+        sum = sum + secondDigit % 10;
+        //  Add firstDigit also in the sum
+        sum = sum + firstDigit;
+        // In the last line divide number again
+        cardNumber = cardNumber / 10;
     }
+    printf("%d \n", sum);
 
+    if (sum % 10 == 0)
+    {
+        printf("Valid\n");
 
-
-    // if (sum % 10 == 0)
-    // {
-
-    //     while (number > 0)
-    //     {
-    //         int firstTwodigit;
-    //         if (number < 100)
-    //         {
-    //             firstTwodigit = number % 100;
-    //             if ((firstTwodigit == 51) ||(firstTwodigit== 52) ||(firstTwodigit== 53) ||(firstTwodigit== 54))
-    //             {
-    //                 printf("American\n");
-    //             }
-    //             else if (firstTwodigit == 34 || firstTwodigit == 37)
-    //             {
-    //                 printf("master\n");
-    //             }
-    //             else if (firstTwodigit == 4)
-    //             {
-    //                 printf("Visa\n");
-    //             }
-    //         }
-    //         number = number / 10;
-    //     }
-
-    // }
-    // else
-    //     {
-    //         printf("invalid\n");
-    //     }
+        while (cardNumber > 0)
+        {
+            int firstTwodigit = 0;
+            if (cardNumber < 100)
+            {
+                firstTwodigit = cardNumber % 100;
+                if (firstTwodigit % 10 == 4)
+                {
+                    printf("Visa\n");
+                }
+                else if ((firstTwodigit == 51) || (firstTwodigit == 52) || (firstTwodigit == 53) || (firstTwodigit == 54) || (firstTwodigit == 55))
+                {
+                    printf("American\n");
+                }
+                else if (firstTwodigit == 34 || firstTwodigit == 37)
+                {
+                    printf("master\n");
+                }
+            }
+            cardNumber = cardNumber / 10;
+        }
+    }
+    else
+    {
+        printf("Invalid\n");
+    }
 }
