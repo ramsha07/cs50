ccd052bcf71e18687c38ebc8669ac65ee71be88a
Ramsha Aijaz <64930853+ramsha07@users.noreply.github.com>
2023-05-14 11:39:57 +0500

    address requested changes

--- week1/credit/credit.c
+++ week1/credit/credit.c
@@ -3,52 +3,48 @@
 
 int main(void)
 {
-    long cardNumber = 378282246310005;
+    long cardNumber = get_long("Number: ");
+    long number = cardNumber;
     int sum = 0;
     while (cardNumber > 0)
     {
-        int firstDigit = cardNumber % 10; // 4
-        cardNumber = cardNumber / 10;     // 123
+        int firstDigit = cardNumber % 10;
+        cardNumber = cardNumber / 10;
         int secondDigit = cardNumber % 10 * 2;
         // Logic for second Digit
         sum = sum + secondDigit % 10;
-        secondDigit = secondDigit / 10;
-        sum = sum + secondDigit % 10;
+        sum = sum + secondDigit / 10;
         //  Add firstDigit also in the sum
         sum = sum + firstDigit;
         // In the last line divide number again
         cardNumber = cardNumber / 10;
     }
-    printf("%d \n", sum);
 
-    if (sum % 10 == 0)
+    if (sum % 10 != 0)
     {
-        printf("Valid\n");
-
-        while (cardNumber > 0)
-        {
-            int firstTwodigit = 0;
-            if (cardNumber < 100)
-            {
-                firstTwodigit = cardNumber % 100;
-                if (firstTwodigit % 10 == 4)
-                {
-                    printf("Visa\n");
-                }
-                else if ((firstTwodigit == 51) || (firstTwodigit == 52) || (firstTwodigit == 53) || (firstTwodigit == 54) || (firstTwodigit == 55))
-                {
-                    printf("American\n");
-                }
-                else if (firstTwodigit == 34 || firstTwodigit == 37)
-                {
-                    printf("master\n");
-                }
-            }
-            cardNumber = cardNumber / 10;
-        }
+        printf("INVALID\n");
+        return 0;
     }
     else
-    {
-        printf("Invalid\n");
-    }
+        while (number > 0)
+        {
+            int firstTwoDigit = 0;
+            if (number < 100)
+            {
+                firstTwoDigit = number % 100;
+                if (firstTwoDigit / 10 == 4)
+                {
+                    printf("VISA\n");
+                }
+                else if ((firstTwoDigit >= 51) && (firstTwoDigit <= 55))
+                {
+                    printf("AMEX\n");
+                }
+                else if (firstTwoDigit == 34 || firstTwoDigit == 37)
+                {
+                    printf("MASTERCARD\n");
+                }
+            }
+            number = number / 10;
+        }
 }
