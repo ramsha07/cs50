6a1caa2c97298c95d5907f14b23c2478a773add7
Ramsha Aijaz <64930853+ramsha07@users.noreply.github.com>
2023-05-14 13:16:35 +0500

    address requested changes

--- week1/credit/credit.c
+++ week1/credit/credit.c
@@ -6,14 +6,17 @@ int main(void)
     long cardNumber = get_long("Number: ");
     long number = cardNumber;
     int sum = 0;
+    int firstTwoDigit = 0;
+    int cardLength = 0;
+
     while (cardNumber > 0)
     {
         int firstDigit = cardNumber % 10;
         cardNumber = cardNumber / 10;
         int secondDigit = cardNumber % 10 * 2;
         // Logic for second Digit
-        sum = sum + secondDigit % 10;
-        sum = sum + secondDigit / 10;
+        sum = sum + secondDigit % 10 + secondDigit / 10;
+        
         //  Add firstDigit also in the sum
         sum = sum + firstDigit;
         // In the last line divide number again
@@ -25,26 +28,31 @@ int main(void)
         printf("INVALID\n");
         return 0;
     }
-    else
-        while (number > 0)
+
+    while (number > 0)
+    {
+        number = number / 10;
+        cardLength++;
+        if (number < 100 && number > 9)
         {
-            int firstTwoDigit = 0;
-            if (number < 100)
-            {
-                firstTwoDigit = number % 100;
-                if (firstTwoDigit / 10 == 4)
-                {
-                    printf("VISA\n");
-                }
-                else if ((firstTwoDigit >= 51) && (firstTwoDigit <= 55))
-                {
-                    printf("AMEX\n");
-                }
-                else if (firstTwoDigit == 34 || firstTwoDigit == 37)
-                {
-                    printf("MASTERCARD\n");
-                }
-            }
-            number = number / 10;
+            firstTwoDigit = number % 100;
         }
+    }
+
+    if (firstTwoDigit / 10 == 4 && (cardLength == 13 || cardLength == 16))
+    {
+        printf("VISA\n");
+    }
+    else if (firstTwoDigit >= 51 && firstTwoDigit <= 55 && cardLength == 16)
+    {
+        printf("MASTERCARD\n");
+    }
+    else if ((firstTwoDigit == 34 || firstTwoDigit == 37) && cardLength == 15)
+    {
+        printf("AMEX\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 }
